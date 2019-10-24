07173397ba1589ed9fae510956061a384f89e2b2
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-10-24 15:28:44 -0400

    add footer and header image

--- react-restaurant/src/App.js
+++ react-restaurant/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from './Header';
+import Nav from './Nav';
 import About from './About';
 import Accordion from './Accordion';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
       <About />
       <Accordion />
+      <Footer />
     </div>
   );
 }
