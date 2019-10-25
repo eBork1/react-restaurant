5cb9aa5f6b81c6c908e1e77ffd0da4ff88e1ee9c
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-10-25 09:43:11 -0400

    update styling

--- react-restaurant/src/Header.js
+++ react-restaurant/src/Header.js
@@ -8,15 +8,17 @@ class Header extends React.Component {
     render() {
         return (
             <header className="header1">
-                <div className="container-fluid p-0">
-                    <div className="row">
-                        <div className="col-12 p-0">
-                            <div className="jumbotron bg-dark">
-                                <p className="p-5 text-light display-3">Bootstrap Bistro</p> 
+                <div className="parallax">
+                    <div className="container-fluid p-0">
+                        <div className="row">
+                            <div className="col-12 p-0">
+                                <div className="jumbotron" id="jumbo">
+                                    <p className="p-5 text-light">Bootstrap Bistro</p>
+                                </div>
+                                <a href="#nav">
+                                    <p className="display-3 text-light mt-5">↓</p>
+                                </a>
                             </div>
-                            <a href="#nav">
-                            <p className="display-3 text-light mt-5">↓</p>
-                            </a>
                         </div>
                     </div>
                 </div>
