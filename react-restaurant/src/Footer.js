cf71a1fb874d92eccb4dbe2bc6613880d0299d93
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-10-25 10:12:08 -0400

    update styling

--- react-restaurant/src/Footer.js
+++ react-restaurant/src/Footer.js
@@ -7,13 +7,11 @@ class Footer extends React.Component {
                 <div className="container-fluid p-0">
                     <div className="row">
                         <div className="col-12">
-                            <div className="jumbotron bg-secondary justify-content-left rounded-0 m-0">
-                                <div className="pl-5">
-                                    <h2 className="text-light text-left">348 E MAIN</h2>
-                                    <h5 className="text-light text-left">Hours of operation:</h5>
-                                    <p className="text-light text-left mb-0">Mon-Thu: 11am-5pm</p>
-                                    <p className="text-light text-left mb-0">Fri-Sat: 11am-8pm</p>
-                                </div>
+                            <div className="jumbotron bg-secondary rounded-0 m-0">
+                                <h2 className="text-light text-center">348 E MAIN</h2>
+                                <h5 className="text-light text-center">Hours of operation:</h5>
+                                <p className="text-light text-center mb-0">Mon-Thu: 11am-5pm</p>
+                                <p className="text-light text-center mb-0">Fri-Sat: 11am-8pm</p>
                             </div>
                         </div>
                     </div>
